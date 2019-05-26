ea7b7c4131a00b08ace9a4d155b539fa33e3a292
Haoran Feng <harrifeng@gmail.com>
2019-05-26 18:57:36 +0800

    bug-1201 branch at 2019-05-26 18:57:36

--- feature-commit.py
+++ feature-commit.py
@@ -6,5 +6,5 @@ for i in range(3):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     os.system('echo {} >> result.txt'.format(ds))
     time.sleep(random.random())
-    os.system('''git commit -a -m 'Circle-Demo branch at {}' '''.format(ds))
+    os.system('''git commit -a -m 'bug-1201 branch at {}' '''.format(ds))
     print("one---------finish---------->>")
