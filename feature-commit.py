698b95c6e569dcb72e3f245471b79588bf15c5a0
Haoran Feng <harrifeng@gmail.com>
2019-05-26 18:58:37 +0800

    Feature 101 branch at 2019-05-26 18:58:36

--- feature-commit.py
+++ feature-commit.py
@@ -2,9 +2,9 @@ import os
 import time
 import datetime
 import random
-for i in range(3):
+for i in range(5):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     os.system('echo {} >> result.txt'.format(ds))
     time.sleep(random.random())
-    os.system('''git commit -a -m 'Circle-Demo branch at {}' '''.format(ds))
+    os.system('''git commit -a -m 'Feature 101 branch at {}' '''.format(ds))
     print("one---------finish---------->>")
